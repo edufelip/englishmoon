04e53de9d60cf66b5a1e0ce5ca1684dac4f4d5a5
eduardo <edu_felip@hotmail.com>
2020-01-09 23:40:47 -0300

    header
    added a hamb menu when in mobile

--- public/frontend.js
+++ public/frontend.js
@@ -1,13 +1,37 @@
-var lis = document.querySelectorAll(".li-color");
+// var lis = document.querySelectorAll("li");
 
-for (var i = 0; i < lis.length; i++) {
-    lis[i].addEventListener("mouseover", function (){
-        this.style.background = "pink";
-    });
-}
-for (var i = 0; i < lis.length; i++) {
-    lis[i].addEventListener("mouseout", function (){
-        this.style.background = "white";
-    });
-}
+// for (var i = 0; i < lis.length; i++) {
+//     lis[i].addEventListener("mouseover", function (){
+//         this.style.background = "pink";
+//     });
+// }
+// for (var i = 0; i < lis.length; i++) {
+//     lis[i].addEventListener("mouseout", function (){
+//         this.style.background = "white";
+//     });
+// }
 
+// let body = document.querySelector(".main");
+// body.addEventListener("mouseover", () => {
+//     body.style.background = "black";
+// })
+
+let hamb = document.querySelector(".hamb-bars");
+let actHamb = document.querySelector(".act-hamb-bars");
+let dropMenu = document.querySelector(".drop-menu");
+let shadow = document.querySelector(".shadow");
+
+hamb.addEventListener("click", () => {
+    // let rect = dropMenu.getBoundingClientRect().left;
+    dropMenu.classList.toggle("drop-translate");
+    // shadow.classList.add("activate-shadow");
+    shadow.style.width = "100vw";
+    shadow.style.background = "#00000030";
+
+})
+actHamb.addEventListener("click", () => {
+    dropMenu.classList.toggle("drop-translate");
+    // shadow.classList.remove("activate-shadow");
+    shadow.style.background = "#00000030";
+    shadow.style.width = "0vw";
+})

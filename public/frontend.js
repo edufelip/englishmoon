b1625f56d41cbc3931811a577ae0d033d1b40f29
eduardo <edu_felip@hotmail.com>
2020-01-10 23:43:38 -0300

    header
    made some huge corrections, there's still things to be done yet

--- public/frontend.js
+++ public/frontend.js
@@ -20,18 +20,75 @@ let hamb = document.querySelector(".hamb-bars");
 let actHamb = document.querySelector(".act-hamb-bars");
 let dropMenu = document.querySelector(".drop-menu");
 let shadow = document.querySelector(".shadow");
+let tinyLog = document.querySelector(".log2");
+let regularLog = document.querySelector(".regular-log");
+let dropAut = document.querySelector(".dropdown-aut");
+let body = document.querySelector(".main");
+let logo = document.querySelector(".logo");
+let options = document.querySelector(".options");
+let search = document.querySelector(".search");
+
+let dropFadeOut = () => {
+    dropMenu.classList.toggle("drop-translate");
+    shadow.style.opacity = "0";
+    setTimeout( () => {
+        shadow.style.visibility = "hidden";
+    }, 300);
+}
+let dropFadeIn = () => {
+    dropMenu.classList.toggle("drop-translate");
+    shadow.style.opacity = "1";
+    shadow.style.visibility = "visible";
+}
+
+let changeAut = () => {
+    if(dropAut.style.visibility == "visible") {
+        dropAut.style.opacity = "0";
+        setTimeout( () => {
+            dropAut.style.visibility = "hidden";
+        }, 200);
+    } else {
+        dropAut.style.visibility = "visible";
+        dropAut.style.opacity = "1";
+    }
+}
+let neutralizeAut = () => {
+    if(dropAut.style.visibility == "visible") {
+        dropAut.style.opacity = "0";
+        setTimeout( () => {
+            dropAut.style.visibility = "hidden";
+        }, 200);
+    }
+}
 
 hamb.addEventListener("click", () => {
     // let rect = dropMenu.getBoundingClientRect().left;
-    dropMenu.classList.toggle("drop-translate");
-    // shadow.classList.add("activate-shadow");
-    shadow.style.width = "100vw";
-    shadow.style.background = "#00000030";
-
+    dropFadeIn();
 })
 actHamb.addEventListener("click", () => {
-    dropMenu.classList.toggle("drop-translate");
-    // shadow.classList.remove("activate-shadow");
-    shadow.style.background = "#00000030";
-    shadow.style.width = "0vw";
+    dropFadeOut();
 })
+shadow.addEventListener("click", () => {
+    dropFadeOut();
+})
+
+tinyLog.addEventListener("click", () => {
+    changeAut();
+})
+regularLog.addEventListener("click", () => {
+    changeAut();
+})
+body.addEventListener("click", () => {
+    neutralizeAut();
+})
+logo.addEventListener("click", () => {
+    neutralizeAut();
+})
+options.addEventListener("click", () => {
+    neutralizeAut();
+})
+search.addEventListener("click", () => {
+    neutralizeAut();
+})
+
+

d0d3bfe5ba7dbfb35c7c3ebccc4f13f6cad11c63
Eduardo <edu_felip@hotmail.com>
2020-04-30 18:37:02 -0300

    articles now only be written and deleted by the admin

--- src/config/islogged.js
+++ src/config/islogged.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 const islogged = {
     isLoggedIn: function(req, res, next){
         if(req.isAuthenticated()){
             return next();
         }
-        res.redirect('/register')
+        return res.redirect('/register')
     },
     isNotLoggedIn: function(req, res, next){
         if(req.isAuthenticated()){
@@ -11,6 +13,12 @@ const islogged = {
         } else {
             return next();
         }
+    },
+    isAdmin: function(req, res, next) {
+        if(req.isAuthenticated() && (req.user.email === process.env.ADMIN_EMAIL || req.user.email === process.env.FAKE_ADMIN_EMAIL)) {
+            return next();
+        }
+        return res.redirect("/")
     }
 }
 

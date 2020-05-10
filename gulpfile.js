4e34bb6adc8481a297174a768c778570d55bdc9c
Eduardo <edu_felip@hotmail.com>
2020-05-10 19:18:25 -0300

    finished gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require("node-sass")
-const cssnano = require('gulp-cssnano');
-const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const img = require('gulp-imagemin')
+const imgmin = require('gulp-imagemin')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify-es').default
 const plumber = require('gulp-plumber')
@@ -13,16 +11,26 @@ const plumber = require('gulp-plumber')
 // src paths
 const js_src = './public/scripts/*.js'
 const sass_src = './public/css/*.scss'
+const img_src = './public/images/*'
+const uploads_src = './public/uploads/*'
 
 // dist paths
 const js_dest = './public'
 const js_dest_name = 'scripts.js'
 const sass_dest = './public'
-const sass_dest_name = 'styles.css'
+const img_dest = './public/images'
+const uploads_dest = './public/uploads'
+
+function imgMinify() {
+  
+}
 
 gulp.task('sass', function(){
   return gulp.src(sass_src)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
     .pipe(gulp.dest(sass_dest))
 })
 
@@ -34,7 +42,22 @@ gulp.task('scripts', function(){
     .pipe(gulp.dest(js_dest))
 })
 
+gulp.task('images', function(){
+  return gulp.src(img_src)
+    .pipe(imgmin())
+    .pipe(gulp.dest(img_dest))
+})
+
+gulp.task('uploads', function(){
+  return gulp.src(uploads_src)
+    .pipe(imgmin())
+    .pipe(gulp.dest(uploads_dest))
+})
+
 gulp.task('default', function(){
-  gulp.watch([js_src], ['scripts'])
+  gulp.watch([js_src], gulp.series('scripts'));
+  gulp.watch([sass_src], gulp.series('sass'));
+  gulp.watch([img_src], gulp.series('images'));
+  gulp.watch([uploads_src], gulp.series('uploads'));
 })
 
--- public/scripts/scriptArticles.js
+++ public/scripts/scriptArticles.js
@@ -1,5 +1,3 @@
-// const main = document.querySelector(".main");
-
 function changeColor() {
     main.style.background = "rgb(243, 243, 243)";
 }
